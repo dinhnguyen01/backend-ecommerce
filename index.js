3cdfb36f6a7e8e9a31d2b090335cf63b99505b49
DinhZing <nguyencongdinhnhb@gmail.com>
2024-06-07 23:23:19 +0700

    first-commit-1.4.4

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+const cors = require("cors");
 const authRouter = require("./routes/authRoute");
 const productRouter = require("./routes/productRoute");
 const blogRouter = require("./routes/blogRoute");
@@ -26,6 +27,7 @@ dbConnect();
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
